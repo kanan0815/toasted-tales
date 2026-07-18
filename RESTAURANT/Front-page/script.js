d7a4594222c3370708a0dd293b6d070610e2b6ab
lilima07 <lilimasingh145@gmail.com>
2026-07-18 13:21:00 +0530

    Add files via upload

---
+++ RESTAURANT/Front-page/script.js
@@ -0,0 +1,9 @@
+const nextBtn = document.getElementById("nextBtn");
+
+nextBtn.addEventListener("click", () => {
+
+    // Later connect this to page 2
+
+    window.location.href = "../MENU/index.html";
+
+});
