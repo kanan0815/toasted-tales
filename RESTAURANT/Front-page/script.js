d103ea5831e2b5030109322c3c37760d3660a368
lilima07 <lilimasingh145@gmail.com>
2026-07-20 19:12:33 +0530

    Delete RESTAURANT directory

--- RESTAURANT/Front-page/script.js
+++
@@ -1,9 +0,0 @@
-const nextBtn = document.getElementById("nextBtn");
-
-nextBtn.addEventListener("click", () => {
-
-    // Later connect this to page 2
-
-    window.location.href = "../MENU/index.html";
-
-});
